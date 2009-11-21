779cb03c5686ec9ea35e0a9f6c3165b1e0105bba
John Ragan <john.ragan@gmail.com>
2009-11-21 11:50:57 -0500

    Added mock object for data fetcher from Fairfax library (currently does the same thing).

---
+++ library_mashable/library/app/models/data_fetcher.rb
@@ -0,0 +1,17 @@
+class DataFetcher
+  def self.fetch_data(title)
+    begin
+      data = Net::HTTP.get(URI.parse(library_url(title)))
+    rescue => err
+      puts "Error2: #{err}"
+      exit
+    end
+  end  
+  
+  private
+
+    def self.library_url(title)
+      encoded_title = CGI::escape(title)
+      "http://fcplcat.fairfaxcounty.gov/uhtbin/cgisirsi/x/0/0/5?searchdata1=#{encoded_title}%7B245%7D&library=ALL&user_id=GUEST&password=1111"
+    end
+end
