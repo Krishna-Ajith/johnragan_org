578ea99b5ec04f5573a748391aa3cdf0bd7b4029
John Ragan <john.ragan@gmail.com>
2009-11-21 07:46:25 -0500

    Added support for string interpolation.

--- library_mashable/library/app/models/search.rb
+++ library_mashable/library/app/models/search.rb
@@ -7,7 +7,7 @@ class Search
       #h = Net::HTTP.new("johnragan.org", 80)
       #resp, data = h.get("2009/11/20/dont-make-me-think-the-rest-of-the-book/", nil)
       encoded_title = CGI::escape(title)
-      data = Net::HTTP.get(URI.parse('http://fcplcat.fairfaxcounty.gov/uhtbin/cgisirsi/x/0/0/5?searchdata1=' + encoded_title + '%7B245%7D&library=ALL&user_id=GUEST&password=1111'))
+      data = Net::HTTP.get(URI.parse("http://fcplcat.fairfaxcounty.gov/uhtbin/cgisirsi/x/0/0/5?searchdata1=#{encoded_title}%7B245%7D&library=ALL&user_id=GUEST&password=1111"))
     rescue => err
       puts "Error2: #{err}"
       exit
