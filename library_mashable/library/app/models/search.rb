5340524ac8f55a45a2e7854ee3382d8696d6bc6a
John Ragan <john.ragan@gmail.com>
2009-11-21 07:44:35 -0500

    Added url encoding to title and tested for it.

--- library_mashable/library/app/models/search.rb
+++ library_mashable/library/app/models/search.rb
@@ -1,11 +1,13 @@
 require "net/http"
+require "cgi"
 
 class Search
   def library_data(title)
     begin
       #h = Net::HTTP.new("johnragan.org", 80)
       #resp, data = h.get("2009/11/20/dont-make-me-think-the-rest-of-the-book/", nil)
-      data = Net::HTTP.get(URI.parse('http://fcplcat.fairfaxcounty.gov/uhtbin/cgisirsi/x/0/0/5?searchdata1=' + title + '%7B245%7D&library=ALL&user_id=GUEST&password=1111'))
+      encoded_title = CGI::escape(title)
+      data = Net::HTTP.get(URI.parse('http://fcplcat.fairfaxcounty.gov/uhtbin/cgisirsi/x/0/0/5?searchdata1=' + encoded_title + '%7B245%7D&library=ALL&user_id=GUEST&password=1111'))
     rescue => err
       puts "Error2: #{err}"
       exit
--- library_mashable/library/test/unit/search_test.rb
+++ library_mashable/library/test/unit/search_test.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 class SearchTest < ActiveSupport::TestCase
   def test_get_library
     search = Search.new
-    data = search.library_data("moscow%20rules")
+    data = search.library_data("moscow rules")
     assert data =~ /moscow rules/
   end
 end
