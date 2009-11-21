bccf164a9e0edaeefd8c4881fec45dae854a9b08
John Ragan <john.ragan@gmail.com>
2009-11-21 11:45:44 -0500

    Missed part of commit.

--- library_mashable/library/app/models/search.rb
+++ library_mashable/library/app/models/search.rb
@@ -26,12 +26,5 @@ class Search
       RAILS_DEFAULT_LOGGER.info e  
     end
     data
-  end
-  
-private
-
-  def library_url(title)
-    encoded_title = CGI::escape(title)
-    "http://fcplcat.fairfaxcounty.gov/uhtbin/cgisirsi/x/0/0/5?searchdata1=#{encoded_title}%7B245%7D&library=ALL&user_id=GUEST&password=1111"
-  end  
+  end 
 end
