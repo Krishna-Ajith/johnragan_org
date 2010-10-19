5abb76d2e71d8527468ac72bbf31684533d1b791
John Ragan <john.ragan@gmail.com>
2010-10-18 20:44:50 -0400

    Added bidirectional support for many to many.

---
+++ Hibernate/BidirectionalManyToMany/src/hello/Category.java
@@ -0,0 +1,38 @@
+package hello;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CATEGORIES")
+public class Category {
+	@Id @GeneratedValue
+	@Column(name = "CATEGORY_ID")
+	private Long id;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Item> items = new HashSet<Item>();
+
+	@Column(name = "CATEGORY_NAME")
+	private String name;
+
+	Category() {}
+
+	Category(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void addItem(Item item) {
+		items.add(item);
+	}
+	
+	public Set<Item> getItems() {
+		return items;
+	}
+}
---
+++ Hibernate/BidirectionalManyToMany/src/hello/HelloWorld.java
@@ -0,0 +1,72 @@
+package hello;
+
+import java.util.*;
+import org.hibernate.*;
+import persistence.*;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// First unit of work
+		Session session =
+			HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		Item item1 = new Item("Book1");
+		Item item2 = new Item("Book2");
+		Item item3 = new Item("Book3");
+		Category category1 = new Category("category 1");
+		Category category2 = new Category("category 2");
+		item1.addCategory(category1);
+		item2.addCategory(category1);
+		item3.addCategory(category1);
+		item2.addCategory(category2);
+		item3.addCategory(category2);
+		Long itemId = (Long) session.save(item1);
+		Long item2Id = (Long) session.save(item2);
+		Long item3Id = (Long) session.save(item3);
+		Long category1Id = (Long) session.save(category1);
+		Long category2Id = (Long) session.save(category2);
+		tx.commit();
+		session.close();
+		
+		// Third unit of work
+		Session newSession3 =
+			HibernateUtil.getSessionFactory().openSession();
+		Transaction newTransaction3 = newSession3.beginTransaction();
+		List items =
+			newSession3.createQuery("from Item i order by i.name asc").list();
+		System.out.println( items.size() +
+			" Items(s) found:" );
+		for ( Iterator iter = items.iterator(); iter.hasNext(); ) {
+			Item loadedItem = (Item) iter.next();
+			System.out.println("Item is: " + loadedItem.getName());
+			for ( Iterator iterCategory = loadedItem.getCategories().iterator(); iterCategory.hasNext(); ) {
+				Category loadedCategory = (Category) iterCategory.next();
+				System.out.println("Category is: " + loadedCategory.getName());
+			}
+		}
+		newTransaction3.commit();
+		newSession3.close();
+		
+		// Fourth unit of work
+		Session newSession4 =
+			HibernateUtil.getSessionFactory().openSession();
+		Transaction newTransaction4 = newSession4.beginTransaction();
+		List categories =
+			newSession4.createQuery("from Category c order by c.name asc").list();
+		System.out.println( categories.size() +
+			" Category(ies) found:" );
+		for ( Iterator iter = categories.iterator(); iter.hasNext(); ) {
+			Category loadedCategory = (Category) iter.next();
+			System.out.println("Category is: " + loadedCategory.getName());
+			for ( Iterator iterItem = loadedCategory.getItems().iterator(); iterItem.hasNext(); ) {
+				Item loadedItem = (Item) iterItem.next();
+				System.out.println("Item is: " + loadedItem.getName());
+			}
+		}
+		newTransaction4.commit();
+		newSession4.close();
+		
+		// Shutting down the application
+		HibernateUtil.shutdown();
+	}
+}
