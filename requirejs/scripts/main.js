7d6b9cbb6372facac4551e3e0c5f55bda0372999
John Ragan <john.ragan@gmail.com>
2012-05-12 18:26:39 -0400

    Made additional changes for require.js

--- requirejs/scripts/main.js
+++ requirejs/scripts/main.js
@@ -4,20 +4,20 @@ require.config({
         "shirt": "src/shirt",
         "logger": "src/logger",
         "alerts": "src/alerts",
-        "jquery": "src/jquery",
-        "alertsv2": "src/alertsv2",
+        "jquery": "src/jquery"
     },
     waitSeconds: 15,
     locale: "en-en"
 });
-define("foo", ["shirt", "logger", "alerts", "jquery", "alertsv2"], function (shirt, logger, alerts, $, alertsv2) {
+define("foo", ["shirt", "logger", "alerts", "jquery"], function (shirt, logger, alerts, $) {
   
 	return {
 		sayShirtColor : function() {
-	        alert("Shirt color is: " + shirt.color + " and title is as follows: " + $('title').text())
-	        alert4();
+	        alert("Shirt color is: " + shirt.color + " and title is as follows: " + $('title').text());
+	        //alert4();
 			logger.logTheShirt();
 			logger.alertv1a2();
+			alert("made it here");
 	    }
 	};
 });
--- requirejs/scripts/src/alertsv2.js
+++ requirejs/scripts/src/alertsv2.js
@@ -1,3 +1,7 @@
 function alert4() {
 	alert("alert 4");
 }
+
+function myInfo() {
+  return "myInfo";
+}
