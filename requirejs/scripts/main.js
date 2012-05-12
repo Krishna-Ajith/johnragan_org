6d4ef480a155a733ce579d5aafd32cbb7f1346b2
John Ragan <john.ragan@gmail.com>
2012-05-12 12:59:50 -0400

    Changed require message

--- requirejs/scripts/main.js
+++ requirejs/scripts/main.js
@@ -1,13 +1,21 @@
-define(["./shirt", "./logger", "./alerts", "./jquery", "alertsv2"], function (shirt, logger, alerts, $, alertsv2) {
+require.config({
+    baseUrl: "scripts",
+    paths: {
+        "shirt": "src/shirt",
+        "logger": "src/logger",
+        "alerts": "src/alerts",
+        "jquery": "src/jquery"
+    },
+    waitSeconds: 15,
+    locale: "en-en"
+});
+define(["shirt", "logger", "alerts", "jquery"], function (shirt, logger, alerts, $) {
 
 	return {
 		sayShirtColor : function() {
 	        alert("Shirt color is: " + shirt.color + " and title is as follows: " + $('title').text());
 			logger.logTheShirt();
-			logger.alertv2a2();
-	    },
-		alert2Out : function() {
-			alert2();
-		}
+			logger.alertv1a2();
+	    }
 	};
 });
