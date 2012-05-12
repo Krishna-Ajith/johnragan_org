e828c95daa1d16116285ef8cedb935fd41ba3f1a
John Ragan <john.ragan@gmail.com>
2012-05-12 13:19:19 -0400

    Added support for new alert type.

--- requirejs/scripts/main.js
+++ requirejs/scripts/main.js
@@ -4,16 +4,18 @@ require.config({
         "shirt": "src/shirt",
         "logger": "src/logger",
         "alerts": "src/alerts",
-        "jquery": "src/jquery"
+        "jquery": "src/jquery",
+        "alertsv2": "src/alertsv2",
     },
     waitSeconds: 15,
     locale: "en-en"
 });
-define(["shirt", "logger", "alerts", "jquery"], function (shirt, logger, alerts, $) {
-
+define("foo", ["shirt", "logger", "alerts", "jquery", "alertsv2"], function (shirt, logger, alerts, $, alertsv2) {
+  
 	return {
 		sayShirtColor : function() {
-	        alert("Shirt color is: " + shirt.color + " and title is as follows: " + $('title').text());
+	        alert("Shirt color is: " + shirt.color + " and title is as follows: " + $('title').text())
+	        alert4();
 			logger.logTheShirt();
 			logger.alertv1a2();
 	    }
--- requirejs/scripts/src/alertsv2.js
+++ requirejs/scripts/src/alertsv2.js
@@ -1,11 +1,3 @@
-function alert1() {
-	alert("alert 1v2");
-}
-
-function alert2() {
-	alert("alert 2v2");
-}
-
-function alert3() {
-	alert("alert 3v2");
+function alert4() {
+	alert("alert 4");
 }
