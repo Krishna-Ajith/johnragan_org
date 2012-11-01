3f1d6fad12dd9ef1d61ab54c758f93236d825d6c
John Ragan <john.ragan@gmail.com>
2012-11-01 18:55:28 -0400

    Additional refactoring of rspec, as well as more work through JRuby.

---
+++ jruby/chapter 2/extending_ruby/fast_version/ackerizer.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'java'
+require 'rubeus'
+
+include Rubeus::Swing
+
+JFrame.new('Ackerizer') do |frame|
+  frame.layout = java.awt.FlowLayout.new
+  
+  @m = JTextField.new '3'
+  @n = JTextField.new '9'
+  
+  JButton.new('->') do
+    @result.text = Java::Ackermann.ack(@m.text.to_i,
+                                 @n.text.to_i).to_s
+  end
+  
+  @result = JTextField.new 10
+  
+  frame.pack
+  frame.show
+end
+                               
--- rspec/codebreaker_top/lib/codebreaker/game.rb
+++ rspec/codebreaker_top/lib/codebreaker/game.rb
@@ -13,16 +13,22 @@ module Codebreaker
     def guess(guess)
       mark = ''
       (0..3).each do |index|
-        if exact_match?(guess, index)
-          mark << '+'
-        end
-      end
-      (0..3).each do |index|
         if number_match?(guess, index)
           mark << '-'
         end  
       end
-      @output.puts mark  
+      
+      @output.puts '+'*exact_match_count(guess) + mark  
+    end
+    
+    def exact_match_count(guess)
+      exact_match_count = 0
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+          exact_match_count += 1
+        end
+      end
+      exact_match_count
     end
     
     def exact_match?(guess, index)
